26486eebfdafe97cfe0f0a60ea805091babbde58
Yari Andres <yari.andres@gmail.com>
2020-02-21 01:17:47 +0100

    adding modal to indv task based on task id

---
+++ trello-application/TASKS.md
@@ -0,0 +1,16 @@
+[x] Part #1: The basic TODO application by columns
+  ### PART #1
+[x] Chose boiler template
+[x] Chose a framework
+[x] Chose a css library
+[x] Make the board cards
+[x] Add modal 
+  
+
+[] Part #2: Responsive behaviours
+  [x]  Show responsive skills
+  [] Show good UX for tablet / mobile
+
+[x] localStorage or local file.
+
+[] Part #3: Interactivity
--- trello-application/src/views/Board.vue
+++ trello-application/src/views/Board.vue
@@ -15,6 +15,7 @@
             class="task"
             v-for="(task, $taskIndex) of column.tasks"
             v-bind:key="$taskIndex"
+            @click="goToTask(task)"
           >
             <span class="w-full flex-no-shrink font-bold">
               {{ task.name }}
@@ -27,6 +28,10 @@
         </div>
       </div>
     </div>
+
+    <div class="task-bg" v-if="isTaskOpen" @click.self="close">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -34,7 +39,20 @@
 import { mapState } from "vuex";
 
 export default {
-  computed: mapState(["board"])
+  computed: {
+    ...mapState(["board"]),
+    isTaskOpen() {
+      return this.$route.name === "task";
+    }
+  },
+  methods: {
+    goToTask(task) {
+      this.$router.push({ name: "task", params: { id: task.id } });
+    },
+    close() {
+      this.$router.push({ name: "board" });
+    }
+  }
 };
 </script>
 
