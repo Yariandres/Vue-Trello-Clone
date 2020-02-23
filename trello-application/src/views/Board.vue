89a965e1247ab4413d6fdeea19e5d25f7b64e371
Yari Andres <yari.andres@gmail.com>
2020-02-23 19:30:33 +0100

    enabling drag and drop task

--- trello-application/src/views/Board.vue
+++ trello-application/src/views/Board.vue
@@ -5,6 +5,9 @@
         class="column"
         v-for="(column, $columnIndex) of board.columns"
         v-bind:key="$columnIndex"
+        @drop="moveTask($event, column.tasks)"
+        @dragover.prevent
+        @dragenter.prevent
       >
         <div class="flex items-center mb-2 font-blod">
           {{ column.name }}
@@ -15,6 +18,8 @@
             class="task"
             v-for="(task, $taskIndex) of column.tasks"
             v-bind:key="$taskIndex"
+            draggable
+            @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
             @click="goToTask(task)"
           >
             <span class="w-full flex-no-shrink font-bold">
@@ -61,6 +66,23 @@ export default {
     createTask(e, tasks) {
       this.$store.commit("CREATE_TASK", { tasks, name: e.target.value });
       e.target.value = "";
+    },
+     pickupTask (e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.setData('task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+    },
+    moveTask (e, toTasks) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+      const taskIndex = e.dataTransfer.getData('task-index')
+    
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        toTasks,
+        taskIndex
+      })
     }
   }
 };
